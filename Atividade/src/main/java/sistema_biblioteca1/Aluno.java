8f715b2b9853c220d4dd65a52758ea96fcb6217a
Gabriel Souza <souzafffoda@gmail.com>
2026-05-25 13:45:34 -0300

    Add files via upload

---
+++ Atividade/src/main/java/sistema_biblioteca1/Aluno.java
@@ -0,0 +1,9 @@
+package sistema_biblioteca1;
+
+public class Aluno {
+        String nome;
+        int matricula;
+        boolean possuiMulta;
+        int livrosEmprestados;
+
+}
---
+++ Atividade/src/main/java/sistema_biblioteca1/Livro.java
@@ -0,0 +1,9 @@
+package sistema_biblioteca1;
+
+public class Livro {
+        String titulo;
+        int codigo;
+        boolean disponivel;
+        int estoque;
+
+    }
